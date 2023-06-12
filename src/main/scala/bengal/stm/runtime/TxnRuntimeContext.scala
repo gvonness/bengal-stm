18714da02e05018f7c5e6abad0b48a3ee4c37aa3
Greg von Nessi <greg.vonnessi@entrolution.ai>
2023-06-12 12:36:26 +0100

    Try to append dirty log updates

--- src/main/scala/bengal/stm/runtime/TxnRuntimeContext.scala
+++ src/main/scala/bengal/stm/runtime/TxnRuntimeContext.scala
@@ -308,7 +308,7 @@ private[stm] trait TxnRuntimeContext[F[_]] {
                    }
                  case TxnResultLogDirty(idClosureRefinement) =>
                    poll {
-                     ex.submitTxnForImmediateRetry(
+                     ex.submitTxn(
                        this.copy(idClosure = idClosureRefinement)
                      )
                    }
